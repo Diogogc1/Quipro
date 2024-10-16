9fadb28ca9f14d71b6b3b87e1970fc175e0aa155
Augusto <augustoribeiro2201@gmail.com>
2024-10-16 11:24:16 -0300

    testando correção de erro

---
+++ frontEnd/src/app/home/quizpage/data.tsx
@@ -0,0 +1,47 @@
+export const quizData = {
+    totalQuestions: 6, 
+    questions: [
+        {
+            text: 'Qual é a fórmula da água?',
+            options: ['H<sub>2</sub>O', 'CO<sub>2</sub>', 'O<sub>2</sub>', 'H<sub>2</sub>'],
+            correctAnswer: 'H<sub>2</sub>O',
+            image: null,
+            explanation: 'A água é formada por dois átomos de hidrogênio e um de oxigênio.',
+        },
+        {
+            text: 'Qual é o símbolo químico do dióxido de carbono?',
+            options: ['CO', 'CO<sub>2</sub>', 'C<sub>2</sub>O', 'COO<sub>2</sub>'],
+            correctAnswer: 'CO<sub>2</sub>',
+            image: 'https://s2.glbimg.com/H8i2w3B3tSv17LfSszZaIrSOa3U=/e.glbimg.com/og/ed/f/original/2018/09/10/industry-1752876_1920.png',
+            explanation: 'O dióxido de carbono é um gás formado por um átomo de carbono e dois de oxigênio.',
+        },
+        {
+            text: 'Qual é o número atômico do hidrogênio?',
+            options: ['1', '2', '3', '4'],
+            correctAnswer: '1',
+            image: null,
+            explanation: 'O hidrogênio tem número atômico 1, sendo o elemento mais leve da tabela periódica.',
+        },
+        {
+            text: 'Qual é o símbolo químico do sódio?',
+            options: ['Na', 'So', 'Sn', 'S'],
+            correctAnswer: 'Na',
+            image: 'https://s2.glbimg.com/00kYXk1d_U33iBfHuFMVuqi8kvs=/620x455/e.glbimg.com/og/ed/f/original/2016/01/20/sal-thinkstock-casa.jpg',
+            explanation: 'O sódio tem símbolo Na, derivado do seu nome em latim, "Natrium".',
+        },
+        {
+            text: 'Qual é a unidade de medida usada para a quantidade de matéria?',
+            options: ['Mol', 'Grama', 'Litro', 'Joule'],
+            correctAnswer: 'Mol',
+            image: null,
+            explanation: 'O mol é a unidade de medida usada para expressar a quantidade de matéria em química.',
+        },
+        {
+            text: 'Qual é o pH de uma solução neutra?',
+            options: ['0', '7', '14', '7.5'],
+            correctAnswer: '7',
+            image: null,
+            explanation: 'Uma solução neutra tem pH igual a 7, o que ocorre na água pura.',
+        }
+    ],
+};
