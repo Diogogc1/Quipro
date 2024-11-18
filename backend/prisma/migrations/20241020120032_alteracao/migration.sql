585dc1d85f365780ab94015c05111838c180a7e0
Matheus Reis <96420412+matchauvers@users.noreply.github.com>
2024-11-18 20:02:10 -0300

    .

--- backend/prisma/migrations/20241020120032_alteracao/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `explanation` to the `quizzes` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "quizzes" ADD COLUMN     "explanation" TEXT NOT NULL;
--- frontEnd/src/DTOs/usuario.ts
+++
@@ -1,5 +0,0 @@
-export default interface Usuario {
-    id: number;
-    email: string;
-    name: string;
-}
