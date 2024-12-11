f9e1d768b0029c8adaec6944abd0bac15d2fbcb7
AlexandreWild9 <117783021+AlexandreWild9@users.noreply.github.com>
2024-12-11 16:10:49 -0300

    Implementação da pagina de pontos

--- frontEnd/src/app/home/trilha/[nivel]/quizpage/[id]/page.tsx
+++ frontEnd/src/app/home/trilha/[nivel]/quizpage/[id]/page.tsx
@@ -1,14 +1,16 @@
-'use client';
-import React, { useState, useEffect } from 'react';
+"use client"
+import React, { useEffect, useState } from 'react';
 import QuizQuestion from '../../../../../components/QuizQuestion';
 import { notFound } from 'next/navigation';
 import { ArrowLeft } from 'phosphor-react';
 import { useRouter } from 'next/navigation';
+import { parseCookies } from "nookies";
+import { useScore } from '@/contexts/ScoreContext';
 
 interface QuizPageProps {
     params: {
         id: string;
-    };
+    }
 }
 
 const QuizPage: React.FC<QuizPageProps> = ({ params }) => {
@@ -16,28 +18,13 @@ const QuizPage: React.FC<QuizPageProps> = ({ params }) => {
     const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
     const [questions, setQuestions] = useState<any[]>([]);
     const [isLoading, setIsLoading] = useState(true);
-    const [score, setScore] = useState(0);
+    const { score, setScore, incrementScore } = useScore();
+    const [currentQuizScore, setCurrentQuizScore] = useState(0);
+
+    const cookies = parseCookies();
+    const userId = cookies.idUser;
+
     const router = useRouter();
-
-    // Função para atualizar pontuação no backend
-    const updateUserPoints = async (userId: number, points: number) => {
-        try {
-            const response = await fetch('http://localhost:3001/usuario/update-user-points', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ userId, points }),
-            });
-
-            if (!response.ok) {
-                throw new Error('Erro ao atualizar pontuação do usuário');
-            }
-            console.log('Pontuação atualizada com sucesso!');
-        } catch (error) {
-            console.error('Erro ao salvar a pontuação:', error);
-        }
-    };
 
     useEffect(() => {
         const fetchQuestions = async () => {
@@ -61,21 +48,39 @@ const QuizPage: React.FC<QuizPageProps> = ({ params }) => {
         };
 
         fetchQuestions();
-    }, [id]);
+    }, [id, userId]);
+
+    const updateUserPoints = async (userId: number, points: number) => {
+        try {
+            const response = await fetch('http://localhost:3001/usuario/update-user-points', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ userId, points }),
+            });
+
+            if (!response.ok) {
+                throw new Error('Erro ao atualizar pontuação do usuário');
+            }
+            console.log('Pontuação atualizada com sucesso!');
+        } catch (error) {
+            console.error('Erro ao salvar a pontuação:', error);
+        }
+    };
 
     const handleNextQuestion = async () => {
         if (currentQuestionIndex + 1 === questions.length) {
-            const userId = 1; // Substitua pelo ID real do usuário autenticado
-            await updateUserPoints(userId, score); // Atualiza pontuação ao finalizar
+            await updateUserPoints(Number(userId), score);
         }
         setCurrentQuestionIndex((prevIndex) => prevIndex + 1);
     };
 
     const handleCorrectAnswer = () => {
-        setScore((prevScore) => prevScore + 1);
+        incrementScore();
+        setCurrentQuizScore((prev) => prev + 1);
     };
 
-    /* Barra de progresso */
     if (isLoading) {
         return (
             <div className="w-full h-full flex justify-center items-center">
@@ -89,9 +94,9 @@ const QuizPage: React.FC<QuizPageProps> = ({ params }) => {
     }
 
     return (
-        <div className="h-full">
+        <div className='h-full'>
             <button onClick={() => router.back()} className="absolute top-24 flex gap-1 items-center sm:top-[7.5rem]">
-                <ArrowLeft size={"1rem"} className="text-zinc-500" />
+                <ArrowLeft size="1rem" className="text-zinc-500" />
                 <p className="text-zinc-500 text-sm leading-snug">Voltar</p>
             </button>
             {questions.length > 0 && currentQuestionIndex < questions.length ? (
@@ -100,18 +105,34 @@ const QuizPage: React.FC<QuizPageProps> = ({ params }) => {
                     onNextQuestion={handleNextQuestion}
                     onCorrectAnswer={handleCorrectAnswer}
                 />
-            ) : currentQuestionIndex >= questions.length ? (
-                <div className="text-center">
-                    <h1>Parabéns! Você completou o quiz.</h1>
-                    <p>Você acertou {score} questões de {questions.length} questões.</p>
-                    <div className="flex items-center justify-center">
-                        <h2>Você ganhou {score} átomos.</h2>
-                        <img src="/assets/atomoIcon.svg" className="h-6" alt="" />
-                    </div>
-                </div>
             ) : (
-                <div className="text-center">
-                    <h2>Sem Conteúdo no momento.</h2>
+                <div className="flex justify-center items-center h-full">
+                    <div className="bg-zinc-800 rounded-lg p-6 w-[500px] text-center">
+                        <img
+                            src="/assets/images/pic_pontuacao.png"
+                            alt="Pontuação"
+                            className="mx-auto mb-6 h-[241px] w-[272px]"
+                        />
+                        <div className='flex justify-center items-center mb-4'>
+                            <img src="/assets/atomoIcon.svg" className="h-6" alt="" />
+                            <h2 className="text-white text-2xl font-bold ml-2">{currentQuizScore} átomos</h2>
+                        </div>
+                        <p className="text-zinc-400">Você acertou {currentQuizScore} questões de {questions.length} questões.</p>
+
+                        <div className="flex flex-col items-center gap-4 mt-6">
+                            <button
+                                className="bg-violet-600 h-[48px] text-white py-2 px-4 rounded-full hover:bg-violet-700"
+                            >
+                                Próximo capítulo
+                            </button>
+                            <button
+                                className="py-2 px-4 rounded-lg text-zinc-400 hover:bg-zinc-700 hover:text-white flex items-center gap-2"
+                            >
+                                <img src="/assets/images/reload_icon.png" className="h-[24px] w-[24px]" alt="" />
+                                Refazer
+                            </button>
+                        </div>
+                    </div>
                 </div>
             )}
         </div>
