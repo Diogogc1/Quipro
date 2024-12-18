d536acc8b5460d7e646ae42c153f3aa2d409d128
Augusto Paiva Ribeiro <augustoribeiro2201@gmail.com>
2024-12-18 08:56:46 -0300

    add .env in front-end

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -19,7 +19,8 @@ export default function Login() {
 
     // Requisição para o back-end
     try {
-      const response = await fetch("http://localhost:3001/usuario/login", {
+      alert(process.env.NEXT_PUBLIC_API_URL);
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/usuario/login`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
